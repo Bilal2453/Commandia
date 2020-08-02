ea55da2a54dae1ce29d2721d7aa417ab5c5dc2a7
Bilal2453 <belal2453@gmail.com>
2020-08-02 21:50:58 +0300

    New system of handling commands
    - Returning a table
    - Retutning a function that will be wrapped as callback
    - Returning a command object using global Command
    - Defining globals (callback, aliases, etc)
    - Merging between multiple ways
    - Injecting a new global command_name that represnts the final name
    - Better logging and error messaging
    - Cleaned code

--- handlers/commandsHandler.lua
+++ handlers/commandsHandler.lua
@@ -1,61 +1,155 @@
-local loader = require '../include/loader'.loadDirec
-local FILES_PATT = '%.command%.lua$'
+local loader = require '../include/loader'.loadDir
+local type = require '../include/utils'.classType
 
-local baseErrorMsg = 'Error loading command "%s" : %s'
+local Command = require '../objects/Command'
+
+--- TODO: Expose FILES_PATT to the end-user, allowing customizing
+local FILES_PATT = '([^%/]+)%.command%.lua$'
+local baseErr = 'Commands Loader (Command "%s") | %s'
 
 return function (manager)
-  local env = setmetatable({
+  local env = {
     manager = manager,
-    Command = function(...) return manager:createCommand(...) end,
-    require = require,
-  }, {
+    require = require, --- NOTE: is this needed anymore?
     __index = _G
-  })
+  }
 
-  ---* Unloading event
-  local function onUnload(n) manager._commands[n] = nil end
+  env.Command = function(cb, perms, aliases, args)
+    return Command(manager, env.command_name,
+      cb or env.cb,
+      perms or env.perms,
+      aliases or env.aliases,
+      args or env.args
+    )
+  end
+  env = setmetatable(env, env)
+
+  local function log(level, n, msg, ...)
+    msg = msg:format(...)
+    manager._logger:log(level, baseErr, n, msg)
+  end
+
+  ---* Before loading event
+  local function onBeforeload(n, l)
+    local loaderEnv = getfenv(l)
+
+    loaderEnv.command_name = n -- Used by Commandia, should not be manually defined
+
+    setfenv(l, loaderEnv)
+  end
 
   ---* Loading event
-  local function onLoad(n, c)
-    if type(c) == 'function' then
+  local function onLoad(n, r, c)
+    local commandEnv, g = getfenv(c), {}
 
-      local success, err = pcall(c, manager)
-      if not success then
-        manager._logger:log(1, baseErrorMsg, n, err); return
-      end
+    local globalsmap = {
+      aliases = {"table", "string"},
 
-      if type(err) == 'table' and err.__name == 'Command' then
-        success, err = pcall(manager.registerCommand, manager, err)
-      elseif err then -- Not nil?
-        manager._logger:log(1, baseErrorMsg, n,
-          ('expected return value of "Command" got "%s"'):format(type(err)))
-        return
-      end
+      permissions = {"table", "string"},
+      perms = {"table", "string"},
 
-      if not success and err then
-        manager._logger:log(1, baseErrorMsg, n, err)
-        return
-      end
+      arguments = {"table"},
+      args = {"table"},
 
-    elseif type(c) == 'table' and c.__name == 'Command' or not c then
+      callback = {"function"},
+      cb = {"function"}
+    }
 
-      local s, e = c and pcall(manager.registerCommand, manager, c)
-
-      if not s and e then
-        manager._logger:log(1, baseErrorMsg, n, e); return
-      end
-
-    else
-      manager._logger:log(1, baseErrorMsg, n, ('expected return value of "Command" got "%s"'):format(type(c)))
-      return
+    for k, _ in pairs(globalsmap) do
+      g[k] = commandEnv[k]
     end
 
-    manager._logger:log(3, 'Successfully loaded "%s" command', n)
+    --- NOTE: In Luvit a global (args) is used instead of the global (arg)
+    -- which might cause conflicting with Commandia's global (args)
+    -- this code will asure no conflicting happens.
+    -- If user defined a global args with index 0 then conflicting will happen
+    -- **an index 0 for `args` must never be defined**.
+    if type(g.args) == "table" and g.args[0] then
+      g.args = nil
+    end
+
+    do -- Check if provided globals are valid
+      local i, isValid
+
+      for global, types in pairs(globalsmap) do
+        i, isValid = g[global]
+
+        for _, t in ipairs(types) do
+          if type(i) == t then isValid = true end
+        end
+
+        if i ~= nil and not isValid then
+          log(1, n, 'bad value for global "%s" (expected %s, got %s)',
+            global, table.concat(types, '|'), type(i)
+          ); return false
+        end
+      end
+    end
+
+    local aliases = g.aliases
+    local perms = g.permissions or g.perms
+    local args = g.arguments or g.args
+    local cb = g.callback or g.cb
+
+    local returnType = type(r)
+    if returnType == 'function' then
+      manager._commands[n] = manager:createCommand(n, r, perms, aliases, args, false)
+    elseif returnType == 'table' then
+
+      manager._commands[n] = manager:createCommand(
+        n, r.callback or cb,
+        r.permissions or r.perms or perms,
+        r.aliases,
+        r.arguments or r.args or args,
+        false
+      )
+
+    elseif returnType == 'Command' then
+      if not r.callback and not cb then
+        log(1, n, 'bad argument #1 to "Command" (expected function|global "callback", got no value)')
+        return false
+      end
+
+      manager._commands[n] = r
+    elseif not r and cb then
+      manager._commands[n] = manager:createCommand(n, cb, perms, aliases, args, false)
+      p(aliases, manager._commands[n].arguments)
+    elseif next(g) then
+      log(1, n, 'A callback is required but got no callback '..
+        '(you can either define a global "callback", and/or return a function value. See wiki for more info.)'
+      ); return false
+
+    else
+      log(1, n, 'bad return value for command "%s" (expected %s, got %s)',
+        n, "function|table|Command", returnType
+      ); return false
+    end
+  end
+
+  ---* First load event
+  local function onFirstload(n)
+    log(3, n, 'Command has been successfully loaded')
+  end
+
+  ---* Reloaded event
+  local function onReloaded(n)
+    log(3, n, 'Command has been successfully reloaded')
+  end
+
+  ---* Unloading event
+  local function onUnload(n)
+    manager._commands[n] = nil
+  end
+
+  ---* Deleting event (renaming/moving/deletion)
+  local function onDeleted(n)
+    log(2, n, 'Command has been unloaded due to Command\'s file inaccessibility')
+    manager._commands[n] = nil
   end
 
   ---* Error event
   local function onErr(n, err)
-    manager._logger:log(1, baseErrorMsg, n, err)
+    log(1, n, err)
   end
 
   -- Load commands and auto-reload on changes
@@ -63,5 +157,9 @@ return function (manager)
     onErr = onErr,
     onLoad = onLoad,
     onUnload = onUnload,
+    onDeleted = onDeleted,
+    onReloaded = onReloaded,
+    onFirstload = onFirstload,
+    onBeforeload = onBeforeload,
   })
 end
